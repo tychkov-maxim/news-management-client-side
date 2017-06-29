8e057d7c381c5eb21238776fd6a8044a4aebcdc4
Maxim Tychkov <tichkov_maxim@mail.ru>
2017-06-29 17:10:10 +0600

    implement getAll and getById methods in NewsService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,35 @@ import {NewsService} from './service/news.service';
 import {News} from './model/news';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html'
+    selector: 'my-app',
+    templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
-  private newsList: News[];
-  private errMsa: string;
+    private newsList: News[];
+    private news: News;
+    private errMsa: string;
 
-  constructor(private newsSerivce: NewsService) {
-  }
+    constructor(private newsSerivce: NewsService) {
+    }
 
-  ngOnInit(): void {
-    this.getAll();
-  }
+    ngOnInit(): void {
+        this.getAll();
+    }
 
 
-  getAll() {
-    this.newsSerivce.getAllNews().subscribe(
-      newsList => this.newsList = newsList,
-      error => this.errMsa = error
-    );
-  }
+    getAll() {
+        this.newsSerivce.getAllNews().subscribe(
+            newsList => this.newsList = newsList,
+            error => this.errMsa = error
+        );
+    }
+
+    getById(id: number) {
+        this.newsSerivce.getNewsById(id).subscribe(
+            news => this.news = news,
+            error => this.errMsa = error
+        );
+
+    }
 }
--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -9,28 +9,35 @@ import {News} from '../model/news';
 @Injectable()
 export class NewsService {
 
-  private AllNewsRestURL = 'http://localhost:1234/rest/news/';
+    private getNewsURL = 'http://localhost:1234/rest/news/';
 
-  constructor(private http: Http) {
-  }
-
-  getAllNews(): Observable<News[]> {
-    return this.http.get(this.AllNewsRestURL)
-      .map(res => <News[]> res.json())
-      .catch(this.handleError);
-  }
-
-  private handleError(error: Response | any) {
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
+    constructor(private http: Http) {
     }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
+
+
+    getNewsById(id: number): Observable<News> {
+        return this.http.get(`${this.getNewsURL}${id}`)
+            .map(resp => resp.json())
+            .catch(this.handleError);
+    }
+
+    getAllNews(): Observable<News[]> {
+        return this.http.get(this.getNewsURL)
+            .map(res => <News[]> res.json())
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 
 }
